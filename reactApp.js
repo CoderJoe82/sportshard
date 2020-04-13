b30114303e874e1e930086ea284256eecdf1427d
CoderJoe82 <coderjoe82@gmail.com>
2020-04-13 17:06:44 -0400

    Finished medium

--- reactApp.js
+++ reactApp.js
@@ -2,6 +2,9 @@ class Team extends React.Component {
   constructor(props) {
     super(props);
 
+    this.shotSound = new Audio("./assets/sounds/shoot.mp3");
+    this.scoreSound = new Audio("./assets/sounds/score.mp3");
+
     this.state = {
       shots: 0,
       score: 0,
@@ -10,9 +13,13 @@ class Team extends React.Component {
 
   handleShot = () => {
     let score = this.state.score;
-
+    this.shotSound.play();
     if (Math.random() > 0.5) {
       score += 1;
+
+      setTimeout(() => {
+        this.scoreSound.play();
+      }, 500);
     }
 
     console.log("shoot!");
@@ -23,6 +30,18 @@ class Team extends React.Component {
   };
 
   render() {
+    let shotPercentageDiv;
+
+    if (this.state.shots) {
+      const shotPercentage = Math.round(
+        (this.state.score / this.state.shots) * 100
+      );
+      shotPercentageDiv = (
+        <div>
+          <strong>Shooting %: </strong> {shotPercentage}
+        </div>
+      );
+    }
     return (
       <div className="Team">
         <h2>{this.props.name}</h2>
@@ -30,7 +49,6 @@ class Team extends React.Component {
           <img
             src={this.props.logo}
             alt={this.props.name}
-            alt="team logo"
             id="imageSizer"
           />
         </div>
@@ -40,39 +58,75 @@ class Team extends React.Component {
         <div>
           <strong>Score:</strong> {this.state.score}
         </div>
+        {shotPercentageDiv}
         <button onClick={this.handleShot}>Shoot!</button>
       </div>
     );
   }
 }
 
-// Deafault App component that all other compents are rendered through
-function App(props) {
+function Game(props) {
   return (
     <React.Fragment>
-      <div id="gameDisplay">
-        <div className="teamHolders">
-          <Team
-            name="Russiaville Raccoons"
-            logo="./assets/images/raccoon.jpg"
-          />
-          <h3>
-            One rummages through your trash
-            <br /> without asking!!!!
-          </h3>
-        </div>
-        <div>
-          <h1>VS</h1>
-        </div>
-        <div className="teamHolders">
-          <Team name="Sheridan Squirrels" logo="./assets/images/squirrel.jpg" />
-          <h3>One tresspasses on your trees daily!!!!</h3>
+      <div>
+        <h1>Welcome to {props.venue}</h1>
+        <div id="gameDisplay">
+          <div className="teamHolders">
+            <Team
+              name={props.visitingTeam.name}
+              logo={props.visitingTeam.logoSrc}
+            />
+            <h3>
+              One rummages through your trash
+              <br /> without asking!!!!
+            </h3>
+          </div>
+          <div>
+            <h1>VS</h1>
+          </div>
+          <div className="teamHolders">
+            <Team
+              name={props.homeTeam.name}
+              logo={props.homeTeam.logoSrc}
+            />
+            <h3>One tresspasses on your trees daily!!!!</h3>
+          </div>
         </div>
       </div>
       <div id="carnageHolder">
         <h1 id="carnageSpot">WHO WILL SURVIVE THE CARNAGE??!!!!</h1>
       </div>
     </React.Fragment>
+  );
+}
+
+// Deafault App component that all other compents are rendered through
+function App(props) {
+  const raccoons = {
+    name: "Russiaville Raccoons",
+    logoSrc: "./assets/images/raccoon.jpg",
+  };
+  const squirrels = {
+    name: "Sheridan Squirrels",
+    logoSrc: "./assets/images/squirrel.jpg",
+  };
+  const bunnies = {
+    name: "Burlington Bunnies",
+    logoSrc: "./assets/images/bunny.png",
+  };
+  const hounds = {
+    name: "Hammond Hounds",
+    logoSrc: "./assets/images/hound.jpg",
+  };
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game venue="Sheridan Arena" homeTeam={bunnies} visitingTeam={hounds} />
+    </div>
   );
 }
 
